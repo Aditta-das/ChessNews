f48250ca00161522dca16ba89f5616dd5efbeaca
Aditta-das <ndas5662@gmail.com>
2025-11-01 20:18:30 +0600

    remove decouple

--- chessnews_project/settings.py
+++ chessnews_project/settings.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 import os
-from decouple import config
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-m%+o!joon=a=(o@4y+94hs#g3i72*0n9sea-a5iv#ru@va0s+#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -276,13 +274,13 @@ CKEDITOR_5_FILE_STORAGE = 'django.core.files.storage.DefaultStorage'
 
 
 # For testing emails in console
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_USE_SSL = config('EMAIL_USE_SSL')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'adittadas00@gmail.com'
+EMAIL_HOST_PASSWORD = 'puqhleijelwcudes'
 
 
